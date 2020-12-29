aa11940b6e3aed02e4376a7a29225c7a1cc6f7d1
KilanQuerville <75904426+KilanQuerville@users.noreply.github.com>
2020-12-29 19:31:02 +0100

    Update

--- bibliotheque/nav.php
+++ bibliotheque/nav.php
@@ -1,7 +1,7 @@
 <nav>
         <ul>
 
-            <li><a href="\biblio_groupe9_etape3\biblio_groupe9_etape3\biblio_groupe9_etape3\bibliotheque\Index.php">Accueil</a></li>
-            <li><a href="\biblio_groupe9_etape3\biblio_groupe9_etape3\biblio_groupe9_etape3\bibliotheque\contact.php">contact</a></li>
+            <li><a href="\biblio_groupe9_etape4\bibliotheque\Index.php">Accueil</a></li>
+            <li><a href="\biblio_groupe9_etape4\bibliotheque\contact.php">contact</a></li>
         </ul>
     </nav>
